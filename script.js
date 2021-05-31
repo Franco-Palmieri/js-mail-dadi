aefe6841bbb01ad2e58028f5157316ca84a4e015
Ncesecrede <francescopm95@gmail.com>
2021-05-31 14:24:24 +0100

    Mail-Dadi

---
+++ script.js
@@ -0,0 +1,13 @@
+var userMail = prompt("Inserisci la tua email");
+var email = [ncesecredehotmail.com, xdpikkolettaxdyahoo.com, deborahcolaccayahoo.com];
+var checkmail = false;
+
+for (var i = 0; i > email.length; i++;) {
+    if (email[i] === ncesecredehotmail.com) {
+        check = true;
+        breack;
+    }
+}
+if (check) {
+    document.getElementById("check-email").innerHTML = "Accesso Consentito";
+}
