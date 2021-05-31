057efa88bcb3be30d37f72e1cd16a8f6ee5fcb8b
Ncesecrede <francescopm95@gmail.com>
2021-05-31 16:12:53 +0100

    Mail-Dadi

--- script.js
+++ script.js
@@ -14,13 +14,16 @@ if (checkMail) {
 }
 
 // ------------ dadi
-num = Math.round(Math.random() * 5) + 1;
+var num = Math.round(Math.random() * 5) + 1;
 document.getElementById("casuale-pc").innerHTML ="Numero casuale del Computer " + num;
-numPlayer = Math.round(Math.random() * 5) + 1;
+var numPlayer = Math.round(Math.random() * 5) + 1;
 document.getElementById("casuale-giocatore").innerHTML ="Numero casuale del Giocatore " + numPlayer;
+var output = document.getElementById("winner");
 
 if (numPlayer > num){
-    document.getElementById("winner").innerHTML ="the Winner is the Player";
-}else (num > numPlayer){
-    document.getElementById("winner").innerHTML ="the Winner is the Computer";
+    output.innerHTML ="the Winner is the Player";
+}else if (num > numPlayer){
+    output.innerHTML ="the Winner is the Computer";
+}else{
+    output.innerHTML ="è patta, rilancia i dadi!!! c:"
 }
