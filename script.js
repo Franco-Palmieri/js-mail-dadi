a4a9ba7e17f98e0499e36615870c0b9a672b1442
Ncesecrede <francescopm95@gmail.com>
2021-05-31 15:08:13 +0100

    Mail-Dadi

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ var userMail = prompt("Inserisci la tua email");
 var email = ["ncesecrede@hotmail.com", "xdpikkolettaxd@yahoo.com", "deborahcolacca@yahoo.com"];
 var checkMail = false;
 
-for (var i = 0; i > email.length; i++;) {
-    if (userMail[i] === email) {
+for (var i = 0; i < email.length; i++) {
+    if (userMail === email[i]) {
         checkMail = true;
     }
 }
 if (checkMail) {
     document.getElementById("check-email").innerHTML = "Accesso Consentito";
 } else {
-    document.getElementById("check-email").innerHTML = "Email Sbagliata"
+    document.getElementById("check-email").innerHTML = "Email Sbagliata";
 }
